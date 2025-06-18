95fae864d131e60853e4b7634bd85b9e23716a50
Tejas4334 <tejasmahajan515@gmail.com>
2025-06-19 00:16:48 +0530

    repositioningg the files and modules

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 
-def get_todo(filename = "files/todos.txt"):
+def get_todo(filename = "todos.txt"):
     """this is to read the text into the file"""
     with open(f"{filename}",'r') as file:
         todos_local = file.readlines()
     return todos_local
 
-def write_todos(todos_arg, filename = "files/todos.txt" ):
+def write_todos(todos_arg, filename = "todos.txt" ):
     """this is to write the text into the file"""
 
     with open(f'{filename}', 'w') as file:
