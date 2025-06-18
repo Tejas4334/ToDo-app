087f8f9d5e99efd728f05dc2730cf02c52a26ea4
Tejas4334 <tejasmahajan515@gmail.com>
2025-06-18 19:18:46 +0530

    implement Edit,Complete,Exit feature

--- gui.py
+++ gui.py
@@ -1,24 +1,32 @@
 import functions
-import FreeSimpleGUI as gui
+import FreeSimpleGUI as Gui
 
-label = gui.Text("Type a TODO")
-input_box = gui.InputText(key = "todo", tooltip="Enter Todo")
-add_button = gui.Button("Add")
-exit_button = gui.Button("Exit")
-edit_button = gui.Button("Edit")
+label = Gui.Text("Type a TODO")
+input_box = Gui.InputText(key ="todo", tooltip="Enter Todo")
+add_button = Gui.Button("Add")
+exit_button = Gui.Button("Exit")
+edit_button = Gui.Button("Edit")
+complete_button = Gui.Button("Complete")
 
-window = gui.Window("To-Do List",
+todo_list = Gui.Listbox(values = functions.get_todo(),
+                        key = "TodoList",
+                        enable_events = True,
+                        size = (45,10),
+                        )
+
+window = Gui.Window("To-Do List",
                     layout = [
                               [label],
                               [input_box,add_button],
-                              [edit_button,exit_button],
+                              [[edit_button,complete_button],todo_list],
+                              [exit_button],
                               ],
                     font = ("Helvetica",12))
 
 while True:
     event,values = window.read()
-    # print(event)
-    # print(values)
+    print(event)
+    print(values)
 
     match event:
         case "Add":
@@ -30,10 +38,23 @@ while True:
 
 
         case "Edit":
-            pass
+            todo_to_edit = values["TodoList"][0]
+            new_todo = values["todo"] + "\n"
+
+            todos = functions.get_todo()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+
+            functions.write_todos(todos)
+
+
         case "Complete":
-            pass
-        case "Exit" | gui.WIN_CLOSED:
+            Complete_todo = values["TodoList"][0]
+            todos = functions.get_todo()
+            todos.remove(Complete_todo)
+            functions.write_todos(todos)
+
+        case "Exit" | Gui.WIN_CLOSED:
             break
 
 window.close()
