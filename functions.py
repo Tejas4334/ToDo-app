9bfafb963ab2056dc3e27f6a5fbb9c4c528151f5
Tejas4334 <tejasmahajan515@gmail.com>
2025-06-18 19:59:33 +0530

    refined functions.py

--- functions.py
+++ functions.py
@@ -1,15 +1,18 @@
 
-
-
 def get_todo(filename = "files/todos.txt"):
+    """this is to read the text into the file"""
     with open(f"{filename}",'r') as file:
         todos_local = file.readlines()
     return todos_local
 
 def write_todos(todos_arg, filename = "files/todos.txt" ):
+    """this is to write the text into the file"""
+
     with open(f'{filename}', 'w') as file:
         file.writelines(todos_arg)
 
-def Update_val(window,Data_stream,Text_key = 'todo',List_key = "TodoList"):
-    window[Text_key].update(value="")
-    window[List_key].update(values=Data_stream)
+def Update_val(window, Data_stream, text_key ='todo', list_key ="TodoList"):
+    """This function is used to clean and update the fields of Todo App"""
+
+    window[text_key].update(value="")             # cleans the text_field
+    window[list_key].update(values=Data_stream)   #updates the TodoList
